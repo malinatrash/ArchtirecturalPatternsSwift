51ce6377b1b92b36b730563c6e5eb48699691ac7
malinatrash <malinatrashh@gmail.com>
2023-01-09 20:18:30 +0800

    обновил ветку main для упрощения работы с MVVM

--- ArchtirecturalPatternsSwift/ViewController.swift
+++ ArchtirecturalPatternsSwift/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var greetingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
 
+    @IBAction func greetingButtonPressed() {
+    }
 }
 
