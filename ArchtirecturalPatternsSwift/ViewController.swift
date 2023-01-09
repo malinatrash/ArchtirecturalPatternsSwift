84bc1f8f3ff99afd7150e4ab065cb8fce920e439
malinatrash <malinatrashh@gmail.com>
2023-01-09 17:07:02 +0800

    Initial Commit

---
+++ ArchtirecturalPatternsSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArchtirecturalPatternsSwift
+//
+//  Created by Pavel Naumov on 09.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
